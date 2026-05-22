acabdf6e3618145f89eb241c2ce15d1b27a50cd7
pamelayva-8 <pamelayva@gmail.com>
2026-05-21 21:15:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title('Hola Mundo!!!')
+def show_header(text_title: str):
+    # Layout: logo + title side by side
+    col1, col2 = st.columns([1, 6])
+    
+    with col1:
+        st.image("https://upload.wikimedia.org/wikipedia/commons/3/32/Universidad_Panamericana_Logo_Dorado.jpg", width=200)
+        
+    with col2:
+        st.title(text_title)
+        st.caption("📘 Developed for: *Business Intelligence (Graduate Level)*")
+        st.caption("Instructor: Edgar Avalos-Gauna (2026), Universidad Panamericana")
+
+show_header('Mi primer tablero en Streamlit')
